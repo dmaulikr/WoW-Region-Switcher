66a11aa42b3f7eff3b70a735a33c6faed8b97b9a
WeZZard <WeZZardLau@gmail.com>
2014-08-15 11:14:37 +0800

    Initial Commit

---
+++ WoW Region Switcher/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  WoW Region Switcher
+//
+//  Created by Manfred Lau on 8/15/14.
+//  Copyright (c) 2014 WeZZard Design. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
---
+++ WoW Region SwitcherTests/WoW_Region_SwitcherTests.m
@@ -0,0 +1,40 @@
+//
+//  WoW_Region_SwitcherTests.m
+//  WoW Region SwitcherTests
+//
+//  Created by Manfred Lau on 8/15/14.
+//  Copyright (c) 2014 WeZZard Design. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <XCTest/XCTest.h>
+
+@interface WoW_Region_SwitcherTests : XCTestCase
+
+@end
+
+@implementation WoW_Region_SwitcherTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
